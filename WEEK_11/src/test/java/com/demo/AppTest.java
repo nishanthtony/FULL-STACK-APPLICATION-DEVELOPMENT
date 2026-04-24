6b1ecd2bb9ff163203c2d1fa3d8d86212af04d21
Nishanth G <vtu24506@veltech.edu.in>
2026-04-24 10:23:20 +0530

    Add files via upload

---
+++ WEEK_11/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
